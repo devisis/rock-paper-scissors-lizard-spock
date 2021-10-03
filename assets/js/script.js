ac777e629c17a7c41666554ddbd4fa2bd337a4a9
DevIsis <kitcher96@gmail.com>
2021-10-03 16:03:20 +0000

    Update calcCPU function and test buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,29 @@
-let player = document.querySelector("#p1score");
-let computer = document.querySelector(".cpuscore");
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+    
+    for (let button of buttons){
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "reset") {
+                alert("reset");
+            } else {
+                let selection = this.getAttribute("data-type")
+                alert (`you selected ${selection}`);
+            }
+        })
+    }
+});
+
+
+function calcCPU() {
+    let options = ["rock", "paper", "scissors", "lizard", "spock"]
+    let rand = Math.floor(Math.random() * 6)
+    return options[rand];
+
+        
+}
+function calcWinner() {
+    
+}
+function updateScore() {}
+function playGame() {}
+function reset() {}
