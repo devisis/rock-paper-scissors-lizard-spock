75e3b99cd6a02e64517ffc14f7431b1143b60e14
DevIsis <kitcher96@gmail.com>
2021-10-09 09:21:29 +0000

    Add missing semicolons

--- assets/js/script.js
+++ assets/js/script.js
@@ -120,7 +120,7 @@ let limit = 3;
 
 select.addEventListener('change', function () {
     limit = parseInt(this.value);
-    reset()
+    reset();
 });
 
 /**
@@ -212,16 +212,16 @@ let span = document.getElementsByClassName("close")[0];
 // When the user clicks on the button, open the modal
 btn.onclick = function () {
     modal.style.display = "block";
-}
+};
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function () {
     modal.style.display = "none";
-}
+};
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
-}
+};
